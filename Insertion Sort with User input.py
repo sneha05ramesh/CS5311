acc0dad49d749e35bd841371e2621ea97845bd70
sneha05ramesh <47774769+sneha05ramesh@users.noreply.github.com>
2024-01-28 23:05:51 -0600

    Add files via upload

---
+++ Insertion Sort with User input.py
@@ -0,0 +1,17 @@
+def insertion_sort(a):
+    for i in range(1,len(a)):
+        hole=i
+        value=a[i]
+        while hole>0 and a[hole-1]>value:
+            a[hole]=a[hole-1]
+            hole=hole-1
+        a[hole]=value
+    print(a)
+y=[]
+n = int(input('Enter how many elements you want: '))
+for j in range(0,n):
+    x=int(input("Enter the elements"))
+    y.append(x)
+insertion_sort(y)
+
+
