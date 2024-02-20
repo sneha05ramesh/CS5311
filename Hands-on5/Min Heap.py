619ecba16b8470d32b76537a98bd2eaf0f016eef
sneha05ramesh <47774769+sneha05ramesh@users.noreply.github.com>
2024-02-20 16:52:29 -0600

    Add files via upload

---
+++ Hands-on5/Min Heap.py
@@ -0,0 +1,60 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def parent(self, i):
+        return (i - 1) >> 1
+
+    def left(self, i):
+        return (i << 1) + 1
+
+    def right(self, i):
+        return (i << 1) + 2
+
+    def heapify(self, i):
+        smallest = i
+        left = self.left(i)
+        right = self.right(i)
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != i:
+            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+            self.heapify(smallest)
+
+    def build_min_heap(self, array):
+        self.heap = array[:]
+        n = len(self.heap)
+        for i in range((n - 1) >> 1, -1, -1):
+            self.heapify(i)
+
+    def pop_root(self):
+        if not self.heap:
+            return None
+        root = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        self.heapify(0)
+        return root
+
+    def insert(self, value):
+        self.heap.append(value)
+        i = len(self.heap) - 1
+        while i > 0 and self.heap[self.parent(i)] > self.heap[i]:
+            self.heap[i], self.heap[self.parent(i)] = self.heap[self.parent(i)], self.heap[i]
+            i = self.parent(i)
+
+    def display(self):
+        print("Min Heap:", self.heap)
+
+
+heap = MinHeap()
+heap.build_min_heap([4, 10, 3, 5, 1])
+heap.display()  # Output: Min Heap: [1, 4, 3, 10, 5]
+heap.insert(2)
+heap.display()  # Output: Min Heap: [1, 4, 2, 5, 10, 3]
+print("Popped root:", heap.pop_root())  # Output: Popped root: 1
+heap.display()  # Output: Min Heap: [2, 4, 3, 5, 10]
