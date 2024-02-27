ffcf55418a84552dbd1978eca3005dc47bae4e29
sneha05ramesh <47774769+sneha05ramesh@users.noreply.github.com>
2024-02-27 16:27:32 -0600

    Add files via upload

---
+++ Hands-on6/QuickSort(random).py
@@ -0,0 +1,24 @@
+import random
+
+def partition(arr, low, high):
+    pivot_index = random.randint(low, high)
+    pivot = arr[pivot_index]
+    arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quicksort(arr, low, pi - 1)
+        quicksort(arr, pi + 1, high)
+
+# Example usage:
+arr = [5,9,10,40,1]
+quicksort(arr, 0, len(arr) - 1)
+print("Sorted array:", arr)
---
+++ Hands-on6/QuickSort.py
@@ -0,0 +1,21 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quicksort(arr, low, pi - 1)
+        quicksort(arr, pi + 1, high)
+
+# Example usage:
+arr = [5,9,10,40,1]
+quicksort(arr, 0, len(arr) - 1)
+print("Sorted array:", arr)
+ 
